66ff625e83049259087cc837d7f89030500fa9ac
MateusProDev <mateusferreiraprodev@gmail.com>
2024-02-16 20:43:52 -0300

    add section dois mobile e section tres com a API do GITHUB para resgatar alguns projetos para a interface do meu portifolio

---
+++ portifolio/src/components/ApiData.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+
+function ApiData() {
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchRepos = async () => {
+            try {
+                const response = await fetch('https://api.github.com/users/MateusProDev/repos');
+                if (!response.ok) {
+                    throw new Error(`Erro ao buscar repositórios: ${response.status}`);
+                }
+                const data = await response.json();
+                const filteredRepos = data.filter(repo => 
+                    repo.name === 'Agencia-corrida-Jc' ||
+                    repo.name === 'Previsao-Meteorologica' ||
+                    repo.name === 'my_portifolio'
+                );
+                setRepos(filteredRepos);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchRepos();
+    }, []);
+
+    if (loading) {
+        return <p>Carregando...</p>;
+    }
+
+    if (error) {
+        return <p>Erro: {error}</p>;
+    }
+
+    return (
+        <div>
+            <h1>Meus Repositórios Selecionados no GitHub</h1>
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.id}>
+                        <h2><a href={repo.html_url} target="_blank" rel="noopener noreferrer">{repo.name}</a></h2>
+                        <p>Data de Criação: {new Date(repo.created_at).toLocaleDateString()}</p>
+                        <p>Linguagem: {repo.language}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ApiData;
--- portifolio/src/components/MainContainer.jsx
+++ portifolio/src/components/MainContainer.jsx
@@ -1,14 +1,15 @@
-import { useRef, useEffect } from 'react';
-import { animated, useSpring } from 'react-spring';
-import './MainContainer.css';
-import './UseScrollAnimation.css'
+import { useRef, useEffect } from "react";
+import { animated, useSpring } from "react-spring";
+import "./MainContainer.css";
+import "./UseScrollAnimation.css";
+import ApiData from "./ApiData";
 
 const useScrollAnimation = (initialTransform) => {
   const elementRef = useRef();
 
   const animation = useSpring({
     opacity: 1,
-    transform: 'translateX(0)',
+    transform: "translateX(0)",
     from: { opacity: 0, transform: initialTransform },
   });
 
@@ -17,7 +18,8 @@ const useScrollAnimation = (initialTransform) => {
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   };
@@ -25,82 +27,112 @@ const useScrollAnimation = (initialTransform) => {
   useEffect(() => {
     const handleScroll = () => {
       if (isElementVisible(elementRef.current)) {
-        elementRef.current.classList.add('animate');
+        elementRef.current.classList.add("animate");
       }
     };
-  
-    window.addEventListener('scroll', handleScroll);
-  
+
+    window.addEventListener("scroll", handleScroll);
+
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
 
   return { elementRef, animation };
 };
 
 const useScrollAnimationLeft = () => {
-  return useScrollAnimation('translateX(-100px)');
+  return useScrollAnimation("translateX(-100px)");
 };
 
 const useScrollAnimationRight = () => {
-  return useScrollAnimation('translateX(100px)');
+  return useScrollAnimation("translateX(100px)");
 };
 
 const useScrollAnimationBottom = () => {
-  return useScrollAnimation('translateY(100px)');
+  return useScrollAnimation("translateY(100px)");
 };
 
 const MainContainer = () => {
-  const { elementRef: elementRefUm, animation: animationUm } = useScrollAnimationLeft();
-  const { elementRef: elementRefDois, animation: animationDois } = useScrollAnimationRight();
-  const { elementRef: elementRefTres, animation: animationTres } = useScrollAnimationBottom();
+  const { elementRef: elementRefUm, animation: animationUm } =
+    useScrollAnimationLeft();
+  const { elementRef: elementRefDois, animation: animationDois } =
+    useScrollAnimationRight();
+  const { elementRef: elementRefTres, animation: animationTres } =
+    useScrollAnimationBottom();
 
   return (
     <>
       <main className="mainApp">
         <div className="mainCenter">
           <section className="section sectionUm">
-            <animated.div ref={elementRefUm} className="animated-element" style={animationUm}>
+            <animated.div
+              ref={elementRefUm}
+              className="animated-element"
+              style={animationUm}
+            >
               <div className="mainSectionsBox secUm">
                 <h2>DESIGNER</h2>
-                <p>Criamos o seu Design com a cara do seu negócio, marcando o mundo digital.</p>
-                <a href="https://wa.me/5585997977730?text=Ol%C3%A1%2C+gostaria+de+mais+informa%C3%A7%C3%B5es+sobre%2C+como+adquirir+um+site."><button>Whatssap</button></a>
+                <p>
+                  Criamos o seu Design com a cara do seu negócio, marcando o
+                  mundo digital.
+                </p>
+                <a href="https://wa.me/5585997977730?text=Ol%C3%A1%2C+gostaria+de+mais+informa%C3%A7%C3%B5es+sobre%2C+como+adquirir+um+site.">
+                  <button>Whatssap</button>
+                </a>
               </div>
             </animated.div>
-            <animated.div ref={elementRefTres} className="animated-element" style={animationTres}>
-            <div className="mainSectionsBox secImg">
-              <img className='imgSecUm' src="/img/gatinho3D.png" alt="Fundo do Site" />
-            </div>
-            </animated.div>
-            <animated.div ref={elementRefDois} className="animated-element" style={animationDois}>
-            <div>
-            <div className="mainSectionsBox secUmDois">
-              <h2>&lt;CODER&gt;</h2>
-              <p>Desenvolvemos seu site a partir do Design que é a cara do seu negócio.</p>
-              <div className="sectionUmSocial">
-                <img src="./img/Instaa.png" alt="Instagram" />
-                <img src="./img/GitHub.png" alt="GitHub" />
-                <img src="./img/TikTok.png" alt="TikTok" />
+            <animated.div
+              ref={elementRefTres}
+              className="animated-element"
+              style={animationTres}
+            >
+              <div className="mainSectionsBox secImg">
+                <img
+                  className="imgSecUm"
+                  src="/img/gatinho3D.png"
+                  alt="Fundo do Site"
+                />
               </div>
-            </div>
-            </div>
+            </animated.div>
+            <animated.div
+              ref={elementRefDois}
+              className="animated-element"
+              style={animationDois}
+            >
+              <div>
+                <div className="mainSectionsBox secUmDois">
+                  <h2>&lt;CODER&gt;</h2>
+                  <p>
+                    Desenvolvemos seu site a partir do Design que é a cara do
+                    seu negócio.
+                  </p>
+                  <div className="sectionUmSocial">
+                    <img src="./img/Instaa.png" alt="Instagram" />
+                    <img src="./img/GitHub.png" alt="GitHub" />
+                    <img src="./img/TikTok.png" alt="TikTok" />
+                  </div>
+                </div>
+              </div>
             </animated.div>
           </section>
           <section className="section sectionDois">
-              <div className="mainSectionsBox secDois">
-                <h2>SEÇÃO</h2>
-                <p>Paragrafo</p>
-              </div>
-            <div className="mainSectionsBox"></div>
+            <div className="mainSectionsBox secDois">
+              <h2>SOBRE MIM</h2>
+              <p>No início de 2023, tomei a decisão de me aventurar no vasto <strong>mundo da programação</strong> e desde então venho me dedicando <strong>intensamente</strong> a aprimorar minhas habilidades em <strong>UI</strong> e <strong>desenvolvimento de software</strong>. Nas horas vagas, encontrar-me diante do teclado, <strong>programando como hobby</strong>, explorando novas tecnologias e desbravando os caminhos do código é o que mais me fascina.</p>
+            </div>
+            <div className="mainSectionsBox secDoisDois">
+              <p>Além disso, adoro <strong>passear com minha esposa</strong>, aproveitando cada momento juntos. Atualmente, estou <strong>imerso no desenvolvimento</strong> de um projeto similar ao <strong>GitHub</strong> para uma pequena comunidade da qual faço parte, conhecida como <strong>APCE.</strong></p>
+            </div>
           </section>
           <section className="section sectionTres">
-              <div className="mainSectionsBox secTres">
-                <h2>SEÇÃO</h2>
-                <p>Paragrafo</p>
-              </div>
-            <div className="mainSectionsBox"></div>
+            <div className="mainSectionsBox secTres">
+              <h2>EXPERIENCE</h2>
+              <ApiData />
+            </div>
+            <div className="mainSectionsBox">
+
+            </div>
           </section>
         </div>
       </main>
